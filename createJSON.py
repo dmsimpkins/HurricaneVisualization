28d68d99ff2d45ae37ade4a76df06aa679fccc24
Daniel Simpkins <run1337@gmail.com>
2012-11-16 03:09:42 -0600

    Added title and more descriptive hovering

--- createJSON.py
+++ createJSON.py
@@ -10,14 +10,14 @@ def main():
 		year= line[0]
 		numStorms = line[1]
 		ace = line[7]
-		jsonFile.write('\n\t{ "name": "' + year + '" , "numStorms": "' + numStorms + '" , "ace": "' + ace + '" ,"children": [\n')
+		jsonFile.write('\n\t{ "name": ' + year + ' , "numStorms": ' + numStorms + ' , "ace": "' + ace + '" ,"children": [\n')
 		for i in range(11,17):
 			cat = i - 11 #give category of hurricane
 			if cat == 0:
 				cat = "tropicalStorm"
 			else:
 				cat = "cat" + str(cat)
-			jsonFile.write('\n\t\t{ "name": "' + cat + '", "number": ' + line[i] + '}')
+			jsonFile.write('\n\t\t{ "name": "' + cat + '", "number": ' + line[i] + ',"year": ' + year + '}')
 			if i != 16:
 				jsonFile.write(',')
 		jsonFile.write('\n\t] },')		
